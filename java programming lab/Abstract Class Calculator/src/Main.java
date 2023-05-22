f58ec59fa98c3d7156929615f3d18223cfcac483
umashankarkavya <115335136+umashankarkavya@users.noreply.github.com>
2023-05-22 15:39:05 +0530

    Add files via upload

---
+++ java programming lab/Abstract Class Calculator/src/Main.java
@@ -0,0 +1,37 @@
+abstract class Calculator {
+    public double calculate(double num1, double num2, String operator) {
+        double result = 0;
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+        }
+        return result;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator() {};
+        double num1 = 10.5;
+        double num2 = 5.2;
+        String operator = "+";
+        double result = calculator.calculate(num1, num2, operator);
+        System.out.println("Result: " + result);
+    }
+}
