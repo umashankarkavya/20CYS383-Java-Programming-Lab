99735ef01eed8fe495438e48242b847b3c6bfa7c
umashankarkavya <115335136+umashankarkavya@users.noreply.github.com>
2023-06-05 17:28:53 +0530

    Add files via upload

---
+++ java_lab_final/src/practise/gui/Sub.java
@@ -0,0 +1,10 @@
+package practise.gui;
+
+public class Sub {
+    public static void main(String[] args) {
+        int x=30;
+        int y=15;
+        int sub=x-y;
+        System.out.println(sub);
+    }
+}
